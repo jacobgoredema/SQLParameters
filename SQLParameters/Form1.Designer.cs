7f43ab20b989a130af6d7ea65e216b6ae95488c5
Jacob <jacobgoredema@gmail.com>
2019-07-30 10:42:34 +0200

    project setup

---
+++ SQLParameters/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SQLParameters
+{
+    partial class frmSqlParameter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRollNumber = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Roll No.:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "City:";
+            // 
+            // txtRollNumber
+            // 
+            this.txtRollNumber.Location = new System.Drawing.Point(91, 19);
+            this.txtRollNumber.Name = "txtRollNumber";
+            this.txtRollNumber.Size = new System.Drawing.Size(151, 20);
+            this.txtRollNumber.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(91, 45);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(151, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(91, 71);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(151, 20);
+            this.txtCity.TabIndex = 5;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(91, 109);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmSqlParameter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 154);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtRollNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmSqlParameter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SQL Parameter";
+            this.Load += new System.EventHandler(this.frmSqlParameter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRollNumber;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ SQLParameters/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLParameters
+{
+    public partial class frmSqlParameter : Form
+    {
+        public frmSqlParameter()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSqlParameter_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        SqlConnection conn;
+        SqlCommand comm;
+
+        string connectionString = "database=student;server=JHB-JACOBGOREDE\\SQLEXPRESS;Trusted_Connection=True;";
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            comm = new SqlCommand();
+            comm.Connection = conn;
+
+            // creating instance of SqlParameter
+            SqlParameter PmtrRollNo = new SqlParameter();
+            PmtrRollNo.ParameterName = "@rn"; //defining name
+            PmtrRollNo.SqlDbType = SqlDbType.Int; //defining datatype
+            PmtrRollNo.Direction = ParameterDirection.Input; // setting the direction
+
+            //Creating instance of SqlParameter
+            SqlParameter PmtrName = new SqlParameter();
+            PmtrName.ParameterName = "@nm"; //defining Name
+            PmtrName.SqlDbType = SqlDbType.VarChar; // defining datatype
+            PmtrName.Direction = ParameterDirection.Input; // setting the direction
+
+            //creating instance of SqlParameter
+            SqlParameter PmrtCity = new SqlParameter();
+
+            PmrtCity.ParameterName = "@ct"; //defining name
+            PmrtCity.SqlDbType = SqlDbType.VarChar; //defining datatype
+            PmrtCity.Direction = ParameterDirection.Input; //setting the direction
+
+            //Adding Parameter instances to SqlCommand
+            comm.Parameters.Add(PmtrRollNo);
+            comm.Parameters.Add(PmtrName);
+            comm.Parameters.Add(PmrtCity);
+
+            // setting values of Parameter
+            PmtrRollNo.Value = Convert.ToInt32(txtRollNumber.Text);
+            PmtrName.Value = txtName.Text;
+            PmrtCity.Value = txtCity.Text;
+
+            comm.CommandText = "INSERT INTO studentDetails VALUES(@rn,@nm,@ct)";
+
+            try
+            {
+                comm.ExecuteNonQuery();
+                MessageBox.Show("Record Saved");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message,"Not Saved");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
+
+
+        }
+    }
+}
